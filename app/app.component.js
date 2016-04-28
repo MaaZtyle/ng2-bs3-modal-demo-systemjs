235797e68f1a031831accb360c9b3f397d231230
Douglas Ludlow <doug.ludlow@royaljay.net>
2016-04-28 12:39:02 -0600

    Update to v0.5.2.

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'ng2-bs3-modal/ng2-bs3-modal'], function(exports_1) {
+System.register(['angular2/core', 'ng2-bs3-modal/ng2-bs3-modal'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -38,7 +40,7 @@ System.register(['angular2/core', 'ng2-bs3-modal/ng2-bs3-modal'], function(expor
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
-            })();
+            }());
             exports_1("AppComponent", AppComponent);
         }
     }
